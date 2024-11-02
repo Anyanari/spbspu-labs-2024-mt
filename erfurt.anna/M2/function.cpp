e9c9ae494df2c05ebc5b20d301c24af62ed5405d
Anyanari <a.erfurt02@mail.ru>
2024-11-02 15:17:44 +0300

    Realize frame-function

--- erfurt.anna/M2/function.cpp
+++ erfurt.anna/M2/function.cpp
@@ -77,6 +77,32 @@ void erfurt::showSet(const std::vector<Set>& sets, std::istream& in, std::ostrea
   }
 }
 
+void erfurt::frameCircle(const std::vector<Circle>& circles, std::istream& in, std::ostream& out)
+{
+  std::string name;
+  in >> name;
+  auto iter = std::find(circles.cbegin(), circles.cend(), Circle{name});
+  if (iter == circles.cend())
+  {
+    throw::std::logic_error("This circle not exist");
+  }
+  else
+  {
+    out << getFrameRectangle(*iter);
+  }
+}
 
-
-
+void erfurt::frameSet(const std::vector<Set>& sets, std::istream& in, std::ostream& out)
+{
+  std::string name;
+  in >> name;
+  auto iter = std::find(sets.cbegin(), sets.cend(), Set{name});
+  if (iter == sets.cend())
+  {
+    throw::std::logic_error("This set not exist");
+  }
+  else
+  {
+    out << getFrameRectangle(*iter);
+  }
+}
--- erfurt.anna/M2/function.hpp
+++ erfurt.anna/M2/function.hpp
@@ -7,4 +7,6 @@ namespace erfurt
   void createSet(std::vector<Set>& sets, const std::vector<Circle>& circles, std::istream& in);
   void showCircle(const std::vector<Circle>& circles, std::istream& in, std::ostream& out);
   void showSet(const std::vector<Set>& sets, std::istream& in, std::ostream& out);
+  void frameCircle(const std::vector<Circle>& circles, std::istream& in, std::ostream& out);
+  void frameSet(const std::vector<Set>& sets, std::istream& in, std::ostream& out);
 } 
